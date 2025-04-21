08076c87e143c1bff3a6bd751bf0c1759a394c79
bella <maekawatakaya10.8@gmail.com>
2025-04-22 06:06:45 +0900

    add other type of mino

--- src/main/scala/com/github/verazza/fx/tetris/logic/Tetromino.scala
+++ src/main/scala/com/github/verazza/fx/tetris/logic/Tetromino.scala
@@ -10,7 +10,65 @@ object Tetromino {
     Array(0, 0, 0, 0)
   )
 
+  val tShape: Array[Array[Int]] = Array(
+    Array(0, 1, 0, 0),
+    Array(1, 1, 1, 0),
+    Array(0, 0, 0, 0),
+    Array(0, 0, 0, 0)
+  )
+
+  val lShape: Array[Array[Int]] = Array(
+    Array(0, 0, 1, 0),
+    Array(1, 1, 1, 0),
+    Array(0, 0, 0, 0),
+    Array(0, 0, 0, 0)
+  )
+
+  val jShape: Array[Array[Int]] = Array(
+    Array(1, 0, 0, 0),
+    Array(1, 1, 1, 0),
+    Array(0, 0, 0, 0),
+    Array(0, 0, 0, 0)
+  )
+
+  val sShape: Array[Array[Int]] = Array(
+    Array(0, 1, 1, 0),
+    Array(1, 1, 0, 0),
+    Array(0, 0, 0, 0),
+    Array(0, 0, 0, 0)
+  )
+
+  val zShape: Array[Array[Int]] = Array(
+    Array(1, 1, 0, 0),
+    Array(0, 1, 1, 0),
+    Array(0, 0, 0, 0),
+    Array(0, 0, 0, 0)
+  )
+
+  val oShape: Array[Array[Int]] = Array(
+    Array(1, 1, 0, 0),
+    Array(1, 1, 0, 0),
+    Array(0, 0, 0, 0),
+    Array(0, 0, 0, 0)
+  )
+
+  val shapes = Map(
+    "I" -> iShape,
+    "T" -> tShape,
+    "L" -> lShape,
+    "J" -> jShape,
+    "S" -> sShape,
+    "Z" -> zShape,
+    "O" -> oShape
+  )
+
   val shapeColors = Map(
-    "I" -> Color.Cyan
+    "I" -> Color.Cyan,
+    "T" -> Color.Purple,
+    "L" -> Color.Orange,
+    "J" -> Color.Blue,
+    "S" -> Color.Green,
+    "Z" -> Color.Red,
+    "O" -> Color.Yellow
   )
 }
