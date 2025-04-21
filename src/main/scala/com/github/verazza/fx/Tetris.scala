781e8164150c005bedc276b1340040ae6aa9da60
bella <maekawatakaya10.8@gmail.com>
2025-04-22 05:05:38 +0900

    display I mino

--- src/main/scala/com/github/verazza/fx/Tetris.scala
+++ src/main/scala/com/github/verazza/fx/Tetris.scala
@@ -3,22 +3,56 @@ package fx
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.Line
+import scalafx.scene.shape.Rectangle
 import scalafx.scene.layout.Pane
+import scalafx.scene.shape.Line
 
 object Tetris extends JFXApp3 {
   val numCols = 10
   val numRows = 20
-  val cellSize = 30
+  val cellSize = 30 // 各セルのサイズ (ピクセル)
   val boardWidth = numCols * cellSize
   val boardHeight = numRows * cellSize
+
+  val iShape: Array[Array[Int]] = Array(
+    Array(0, 0, 0, 0),
+    Array(1, 1, 1, 1),
+    Array(0, 0, 0, 0),
+    Array(0, 0, 0, 0)
+  )
+
+  val shapeColors = Map(
+    "I" -> Color.Cyan
+  )
 
   override def start(): Unit = {
     stage = new JFXApp3.PrimaryStage {
       title.value = "ScalaFX Tetris"
       scene = new Scene(boardWidth, boardHeight) {
         fill = Color.LightGray
-        val gridLines = new Pane {
+        val gamePane = new Pane {
+          val currentTetrominoShape: Array[Array[Int]] = iShape
+          val color = shapeColors("I")
+          val startX = numCols / 2 - 2
+          val startY = 0
+
+          for (
+            row <- currentTetrominoShape.indices;
+            col <- currentTetrominoShape(row).indices
+          ) {
+            if (currentTetrominoShape(row)(col) == 1) {
+              val rect = new Rectangle {
+                x = (startX + col) * cellSize
+                y = (startY + row) * cellSize
+                width = cellSize
+                height = cellSize
+                fill = color
+                stroke = Color.Black
+              }
+              children += rect
+            }
+          }
+
           for (i <- 0 to numCols) {
             children += new Line {
               startX = i * cellSize
@@ -38,7 +72,7 @@ object Tetris extends JFXApp3 {
             }
           }
         }
-        content = gridLines
+        content = gamePane
       }
     }
   }
