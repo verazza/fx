5dec1d2c59336935ab1487d3b05fd0ba94a23794
bella <maekawatakaya10.8@gmail.com>
2025-04-22 04:35:30 +0900

    add grid-lines window

--- src/main/scala/com/github/verazza/fx/Tetris.scala
+++ src/main/scala/com/github/verazza/fx/Tetris.scala
@@ -3,21 +3,42 @@ package fx
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.Rectangle
+import scalafx.scene.shape.Line
+import scalafx.scene.layout.Pane
 
 object Tetris extends JFXApp3 {
+  val numCols = 10
+  val numRows = 20
+  val cellSize = 30
+  val boardWidth = numCols * cellSize
+  val boardHeight = numRows * cellSize
+
   override def start(): Unit = {
     stage = new JFXApp3.PrimaryStage {
       title.value = "ScalaFX Tetris"
-      scene = new Scene(300, 400) {
+      scene = new Scene(boardWidth, boardHeight) {
         fill = Color.LightGray
-        content = new Rectangle {
-          x = 50
-          y = 50
-          width = 100
-          height = 100
-          fill = Color.Red
+        val gridLines = new Pane {
+          for (i <- 0 to numCols) {
+            children += new Line {
+              startX = i * cellSize
+              startY = 0
+              endX = i * cellSize
+              endY = boardHeight
+              stroke = Color.Gray
+            }
+          }
+          for (i <- 0 to numRows) {
+            children += new Line {
+              startX = 0
+              startY = i * cellSize
+              endX = boardWidth
+              endY = i * cellSize
+              stroke = Color.Gray
+            }
+          }
         }
+        content = gridLines
       }
     }
   }
